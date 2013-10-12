e1f58fccc65d6ccedaeb600bd34abbfcf3e15e99
Iliya Rachev <rachev.id@gmail.com>
2013-10-12 04:07:32 -0500

    Changes made by Iliya Rachev (rachev.id@gmail.com).

--- sky-trails/scripts/location.js
+++ sky-trails/scripts/location.js
@@ -1,8 +1,8 @@
 (function (global) {
     var map,
-        geocoder,
-        LocationViewModel,
-        app = global.app = global.app || {};
+    geocoder,
+    LocationViewModel,
+    app = global.app = global.app || {};
 
     LocationViewModel = kendo.data.ObservableObject.extend({
         _lastMarker: null,
@@ -11,7 +11,7 @@
 
         onNavigateHome: function () {
             var that = this,
-                position;
+            position;
 
             that._isLoading = true;
             that.showLoading();
@@ -22,25 +22,27 @@
                     map.panTo(position);
                     that._putMarker(position);
 
+                    //that.codeLatLng(position.coords.latitude, position.coords.longitude);
+                    
                     that._isLoading = false;
                     that.hideLoading();
                 },
                 function (error) {
                     //default map coordinates                    
-                    position = new google.maps.LatLng(43.459336, -80.462494);
-                    map.panTo(position);
-
+                    //position = new google.maps.LatLng(43.459336, -80.462494);
+                    //map.panTo(position);
                     that._isLoading = false;
                     that.hideLoading();
 
                     navigator.notification.alert("Unable to determine current location. Cannot connect to GPS satellite.",
-                        function () { }, "Location failed", 'OK');
+                                                 function () {
+                                                 }, "Location failed", 'OK');
                 },
                 {
-                    timeout: 30000,
-                    enableHighAccuracy: true
-                }
-            );
+                timeout: 30000,
+                enableHighAccuracy: true
+            }
+                );
         },
 
         onSearchAddress: function () {
@@ -48,12 +50,13 @@
 
             geocoder.geocode(
                 {
-                    'address': that.get("address")
-                },
+                'address': that.get("address")
+            },
                 function (results, status) {
                     if (status !== google.maps.GeocoderStatus.OK) {
                         navigator.notification.alert("Unable to find address.",
-                            function () { }, "Search failed", 'OK');
+                                                     function () {
+                                                     }, "Search failed", 'OK');
 
                         return;
                     }
@@ -84,22 +87,54 @@
                 map: map,
                 position: position
             });
+        },
+        
+        codeLatLng: function(lat, lng) {
+            var latlng = new google.maps.LatLng(lat, lng);
+            geocoder.geocode({'latLng': latlng}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    console.log(results)
+                    if (results[1]) {
+                        //formatted address
+                        alert(results[0].formatted_address)
+                        //find country name
+                        for (var i = 0; i < results[0].address_components.length; i++) {
+                            for (var b = 0;b < results[0].address_components[i].types.length;b++) {
+                                //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                                    //this is the object you are looking for
+                                    city = results[0].address_components[i];
+                                    break;
+                                }
+                            }
+                        }
+                        //city data
+                        alert(city.short_name + " " + city.long_name)
+                    }
+                    else {
+                        alert("No results found");
+                    }
+                }
+                else {
+                    alert("Geocoder failed due to: " + status);
+                }
+            });
         }
     });
 
     app.locationService = {
         initLocation: function () {
             var mapOptions = {
-                    zoom: 15,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    zoomControl: true,
-                    zoomControlOptions: {
-                        position: google.maps.ControlPosition.LEFT_BOTTOM
-                    },
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                zoomControl: true,
+                zoomControlOptions: {
+                    position: google.maps.ControlPosition.LEFT_BOTTOM
+                },
     
-                    mapTypeControl: false,
-                    streetViewControl: false
-                };
+                mapTypeControl: false,
+                streetViewControl: false
+            };
 
             map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);            
             geocoder = new google.maps.Geocoder();
