a538fe23b679d8ae1c316863a80ba2aca7fd47a3
Iliya Rachev <rachev.id@gmail.com>
2013-10-13 23:11:17 -0500

    get data from Everlive DB by Iliya Rachev

--- sky-trails/scripts/app.js
+++ sky-trails/scripts/app.js
@@ -1,8 +1,16 @@
 (function (global) {
-    
     app = global.app = global.app || {};
 
     document.addEventListener("deviceready", function () {
         app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout", transition: "fade" });
     }, false);
+    
+    var applicationSettings = {
+        apiKey: 'wWPalgivRBMkPWo1'
+    };
+    // initialize Icenium Everlive SDK
+    var el = new Everlive({
+        apiKey: applicationSettings.apiKey
+    });
+    
 })(window);
--- sky-trails/scripts/trails-tracker.js
+++ sky-trails/scripts/trails-tracker.js
@@ -1,23 +1,97 @@
 (function(global) {  
     var TrailsTrackerViewModel,
-        app = global.app = global.app || {};
+    el, mimeMap,
+    AppHelper,
+    app = global.app = global.app || {};
+    
+    el = new Everlive({
+        apiKey: "wWPalgivRBMkPWo1"
+    });
+    
+    mimeMap = {
+        jpg  : "image/jpeg",
+        jpeg : "image/jpeg",
+        png  : "image/png",
+        gif  : "image/gif"
+    };
+    
+    AppHelper = {
+        resolveImageUrl: function (id) {
+            if (id) {
+                return el.Files.getDownloadUrl(id);
+            }
+            else {
+                return "_";
+            }
+        },
+        getBase64ImageFromInput : function (input, cb) {
+            var reader = new FileReader();
+            reader.onloadend = function (e) {
+                if (cb)
+                    cb(e.target.result);
+            };
+            reader.readAsDataURL(input);
+        },
+        getImageFileObject: function(input, cb) {
+            var name = input.name;
+            var ext = name.substr(name.lastIndexOf('.') + 1);
+            var mimeType = mimeMap[ext];
+            if (mimeType) {
+                this.getBase64ImageFromInput(input, function(base64) {
+                    var res = {
+                        "Filename"    : name,
+                        "ContentType" : mimeType,              
+                        "base64"      : base64.substr(base64.lastIndexOf('base64,') + 7)
+                    }
+                    cb(null, res);
+                });
+            }
+            else {
+                cb("File type not supported: " + ext);    
+            }
+        }
+    };
     
     TrailsTrackerViewModel = kendo.data.ObservableObject.extend({
         trailsDataSource: null,
+        trailsModel: {
+            id: 'Id',
+            fields: {
+                CreatedAt: {
+                    defaultValue: ''
+                },
+                ImageFile: {
+                    defaultValue: ''
+                }
+            },
+            PictureUrl: function () {
+                return AppHelper.resolveImageUrl(this.get('ImageFile'));
+            }
+        },
         
         init: function () {
             var that = this,
-                dataSource;
+            dataSource;
             
             kendo.data.ObservableObject.fn.init.apply(that, []);
             
             dataSource = new kendo.data.DataSource({
+                type: 'everlive',
+                schema: {
+                    model: that.trailsModel
+                },
                 transport: {
-                    read: {
-                        url: "data/trails.json",
-                        dataType: "json"
+                    typeName: 'TrailsPictures'
+                },
+                change: function (e) {
+                    if (e.items && e.items.length > 0) {
+                        $('#no-images-span').hide();
                     }
-                }
+                    else {
+                        $('#no-images-span').show();
+                    }
+                },
+                sort: { field: 'CreatedAt', dir: 'desc' }
             });
             
             that.set("trailsDataSource", dataSource);           
